ac8b48bcb176b26f7cf9f6b45d2fe1a92f8986e8
Audrey Randall <aura6466@elra-01.cs.colorado.edu>
2016-09-22 20:29:14 -0600

    forgot to add stuff

---
+++ hw3-oops.c
@@ -0,0 +1,310 @@
+/*
+  Audrey Randall's homework 2
+  Citations:
+    example 26 from class
+    http://www.opengl-tutorial.org/beginners-tutorials/tutorial-4-a-colored-cube/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+//#include "CSCIx229.h"
+//  OpenGL with prototypes for glext
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+double w2h;
+double cubeRotate = 0;
+int th=0;       // Azimuth of view angle
+int ph=0;       // Elevation of view angle
+double z=0;     // Z variable
+double w=1;     // W variable
+double dim=2;   // Dimension of orthogonal box
+char* text[] = {"","2D","3D constant Z","3D","4D"};  // Dimension display text
+int obj; //loads my elf dude that I made in Sculptris. "Object display list."
+int shouldMove = 1;
+//More things I copied from example 26!
+float RGBA[4] = {1,1,1,1};
+#define LEN 8192  // Maximum length of text string
+#define PI 3.1415926
+
+void Print(const char* format , ...)
+{
+   char    buf[LEN];
+   char*   ch=buf;
+   va_list args;
+   //  Turn the parameters into a character string
+   va_start(args,format);
+   vsnprintf(buf,LEN,format,args);
+   va_end(args);
+   //  Display the characters one at a time at the current raster position
+   while (*ch)
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
+
+/*
+ *  GLUT calls this routine when a key is pressed
+ */
+void key(unsigned char ch,int x,int y)
+{
+   //  Exit on ESC
+   if (ch == 27)
+      exit(0);
+   //  Reset view angle
+   else if (ch == '0')
+      th = ph = 0;
+   //  Increase w by 0.1
+   else if (ch == '+')
+   {
+      w += 0.1;
+   }
+   //  Decrease w by 0.1
+   else if (ch == '-')
+   {
+      w -= 0.1;
+   }
+   else if(ch == 's') {
+     shouldMove=!shouldMove;
+   }
+
+   Project(0,w2h,dim);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void special(int key,int x,int y)
+{
+   //  Right arrow key - increase azimuth by 5 degrees
+   if (key == GLUT_KEY_RIGHT)
+      th += 5;
+   //  Left arrow key - decrease azimuth by 5 degrees
+   else if (key == GLUT_KEY_LEFT)
+      th -= 5;
+   //  Up arrow key - increase elevation by 5 degrees
+   else if (key == GLUT_KEY_UP)
+      ph -= 5;
+   //  Down arrow key - decrease elevation by 5 degrees
+   else if (key == GLUT_KEY_DOWN)
+      ph += 5;
+    //  PageUp key - increase dim
+    else if (key == GLUT_KEY_PAGE_DOWN)
+       dim += 0.1;
+    //  PageDown key - decrease dim
+    else if (key == GLUT_KEY_PAGE_UP && dim>1)
+       dim -= 0.1;
+   //  Keep angles to +/-360 degrees
+   th %= 360;
+   ph %= 360;
+
+   Project(0,w2h,dim);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+   //  Ratio of the width to the height of the window
+   w2h = (height>0) ? (double)width/height : 1;
+   //  Set the viewport to the entire window
+   glViewport(0,0, width,height);
+   //  Tell OpenGL we want to manipulate the projection matrix
+   Project(0, w2h, dim);
+}
+
+void drawCube() {
+    glBegin(GL_TRIANGLES);
+    glColor3f(1,1,0); //yellow
+    glNormal3f(-1,0,0);
+    glVertex3d(-1.0f,-1.0f,-1.0f);
+    glVertex3d(-1.0f,-1.0f, 1.0f);
+    glVertex3d(-1.0f, 1.0f, 1.0f);
+    glVertex3d(-1.0f,-1.0f,-1.0f);
+    glVertex3d(-1.0f, 1.0f, 1.0f);
+    glVertex3d(-1.0f, 1.0f,-1.0f);
+
+    glNormal3f(0,0,-1);
+    glColor3f(1,0,0); //red
+    glVertex3d(1.0f, 1.0f,-1.0f);
+    glVertex3d(-1.0f,-1.0f,-1.0f);
+    glVertex3d(-1.0f, 1.0f,-1.0f);
+    glVertex3d(1.0f, 1.0f,-1.0f);
+    glVertex3d(1.0f,-1.0f,-1.0f);
+    glVertex3d(-1.0f,-1.0f,-1.0f);
+
+    glColor3f(0,1,0); //green
+    glVertex3d(1.0f,-1.0f, 1.0f);
+    glVertex3d(-1.0f,-1.0f,-1.0f);
+    glVertex3d(1.0f,-1.0f,-1.0f);
+    glVertex3d(1.0f,-1.0f, 1.0f);
+    glVertex3d(-1.0f,-1.0f, 1.0f);
+    glVertex3d(-1.0f,-1.0f,-1.0f);
+
+    glColor3f(0,0,1); //blue correct
+    glVertex3d(-1.0f, 1.0f, 1.0f);
+    glVertex3d(-1.0f,-1.0f, 1.0f);
+    glVertex3d(1.0f,-1.0f, 1.0f);
+    glVertex3d(1.0f, 1.0f, 1.0f);
+    glVertex3d(-1.0f, 1.0f, 1.0f);
+    glVertex3d(1.0f,-1.0f, 1.0f);
+
+    glColor3f(1,0,1); //magenta
+    glVertex3d(1.0f,-1.0f,-1.0f);
+    glVertex3d(1.0f, 1.0f, 1.0f);
+    glVertex3d(1.0f,-1.0f, 1.0f);
+    glVertex3d(1.0f, 1.0f, 1.0f);
+    glVertex3d(1.0f,-1.0f,-1.0f);
+    glVertex3d(1.0f, 1.0f,-1.0f);
+
+    glColor3f(0,1,1); //cyan correct
+    glVertex3d(1.0f, 1.0f, 1.0f);
+    glVertex3d(-1.0f, 1.0f,-1.0f);
+    glVertex3d(-1.0f, 1.0f, 1.0f);
+    glVertex3d(1.0f, 1.0f, 1.0f);
+    glVertex3d(1.0f, 1.0f,-1.0f);
+    glVertex3d(-1.0f, 1.0f,-1.0f);
+
+    glEnd();
+    glFlush();
+}
+
+void drawAxes(){
+  //Draw axes
+  glColor3f(1,1,1);
+  glBegin(GL_LINES);
+  glVertex3d(0,0,0);
+  glVertex3d(1,0,0);
+  glVertex3d(0,0,0);
+  glVertex3d(0,1,0);
+  glVertex3d(0,0,0);
+  glVertex3d(0,0,1);
+  glEnd();
+}
+
+void display() {
+  //printf("display \n");
+
+  //Lighting variables, taken from example 26
+  float Emission[]  = {0.0,0.0,0.0,1.0};
+  float Ambient[]   = {0.3,0.3,0.3,1.0};
+  float Diffuse[]   = {1.0,1.0,1.0,1.0};
+  float Specular[]  = {1.0,1.0,1.0,1.0};
+  float Position[]  = {0.9,0.9,0.9};//{2*Cos(zh),Ylight,2*Sin(zh),1.0};
+  float Shinyness[] = {16};
+  //This needs to be called before glClear for reasons unknown!!!!!!!!!!!
+  glEnable(GL_DEPTH_TEST);
+  //  Clear the image
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  //  Reset previous transforms
+  glLoadIdentity();
+  //  Set view angle
+  glRotated(ph,1,0,0);
+  glRotated(th,0,1,0);
+
+  glEnable(GL_NORMALIZE); //Normalizes all the normal vectors.
+  glEnable(GL_LIGHTING);
+  //  Enable light 0
+  glEnable(GL_LIGHT0);
+  //  Set ambient, diffuse, specular components and position of light 0
+  glLightfv(GL_LIGHT0,GL_AMBIENT ,Ambient);
+  glLightfv(GL_LIGHT0,GL_DIFFUSE ,Diffuse);
+  glLightfv(GL_LIGHT0,GL_SPECULAR,Specular);
+  glLightfv(GL_LIGHT0,GL_POSITION,Position);
+  //  Set materials
+  glMaterialfv(GL_FRONT_AND_BACK,GL_SHININESS,Shinyness);
+  glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT,RGBA);
+  glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE,RGBA);
+  glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,Specular);
+  glMaterialfv(GL_FRONT_AND_BACK,GL_EMISSION,Emission);
+
+  //Draw elf dude!
+  glPushMatrix();
+  glRotated(180,0,1,0);
+  glScaled(0.1,0.1,0.1);
+  glCallList(obj);
+  glPopMatrix();
+  //Allow regular color to work again
+  glDisable(GL_LIGHTING);
+  //Draw some cubes. Why don't the lighting and mats etc. apply here if GL_LIGHTING is still enabled?
+  double rad = 1.6;
+  double i;
+  //printf("cubeRotate is %f \n", cubeRotate);
+  for(i = 0; i < (2*PI); i+=(PI/4)) {
+    glPushMatrix();
+    glTranslated(rad*cos(i),rad*sin(i),0);
+    glScaled(0.1,0.1,0.1);
+    glRotated(cubeRotate, cos(i), sin(i), 0);
+    drawCube();
+    glPopMatrix();
+  }
+
+  //drawAxes();
+  glFlush();
+  glutSwapBuffers(); //this is for double buffered window. Single buffered uses glFlush.
+}
+
+//Projection function, mostly stolen as usual from Example 26
+void Project(double fov,double asp,double dim)
+{
+   //  Tell OpenGL we want to manipulate the projection matrix
+   glMatrixMode(GL_PROJECTION);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Perspective transformation
+   if (fov)
+      gluPerspective(fov,asp,dim/16,16*dim);
+   //  Orthogonal transformation
+   else
+      glOrtho(-asp*dim,asp*dim,-dim,+dim,-dim,+dim);
+   //  Switch to manipulating the model matrix
+   glMatrixMode(GL_MODELVIEW);
+   //  Undo previous transformations
+   glLoadIdentity();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void idle()
+{
+   //  Elapsed time in seconds
+   double t = glutGet(GLUT_ELAPSED_TIME)/1000.0;
+   if(shouldMove) cubeRotate = fmod(100*t,360.0);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+int main(int argc,char* argv[])
+{
+  //  Initialize GLUT and process user parameters
+   glutInit(&argc,argv);
+   //  Request double buffered, true color window with z-buffering
+   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE| GLUT_DEPTH);
+   glEnable(GL_DEPTH_TEST);
+
+   //  Request 500 x 500 pixel window
+   glutInitWindowSize(500,500);
+   //  Create the window
+   glutCreateWindow("Elvish Magician");
+
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutSpecialFunc(special);
+   glutKeyboardFunc(key);
+   glutIdleFunc(idle);
+   //obj = LoadOBJ("elf_obj.obj");
+   //  Pass control to GLUT so it can interact with the user
+   glutMainLoop();
+   //  Return code
+   return 0;
+}
