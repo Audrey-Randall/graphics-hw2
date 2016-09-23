7099b0efb7a08c91bf95ac84998c22a27ac05b7e
Audrey Randall <audrey.randall@colorado.edu>
2016-09-22 19:32:17 -0600

    Turns out this assignment is designed to teach us how to draw objects WITHOUT using object files. My bad. The elf magician has been replaced with a sea urchin magician. Or possibly an elder god. Whatever you like.

--- hw2.c
+++ hw2.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <time.h>
 #include "CSCIx229.h"
 //  OpenGL with prototypes for glext
 #define GL_GLEXT_PROTOTYPES
@@ -29,8 +30,14 @@ int obj; //loads my elf dude that I made in Sculptris. "Object display list."
 int shouldMove = 1;
 //More things I copied from example 26!
 float RGBA[4] = {1,1,1,1};
+int spineAnglesSet = 0;
+#define SPINES 100
+double spineAngles[SPINES*4];
 #define LEN 8192  // Maximum length of text string
 #define PI 3.1415926
+//those were here already?
+//#define Sin(theta) (sin(PI*theta/180))
+//#define Cos(theta) (cos(PI*theta/180))
 
 void Print(const char* format , ...)
 {
@@ -44,6 +51,31 @@ void Print(const char* format , ...)
    //  Display the characters one at a time at the current raster position
    while (*ch)
       glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
+
+void sphericalToCartesian(double rho, double phi, double theta, double* x, double* y, double* z) {
+  *x = rho*Sin(theta)*Cos(phi);
+  *y = rho*Sin(theta)*Cos(phi);
+  *z = rho*Cos(theta);
+}
+
+void drawCone(double h, double r) {
+  glBegin(GL_TRIANGLE_FAN);
+  glNormal3f(0,0,1);
+  glVertex3d(0,0,h);
+  int i;
+  for(i = 0; i <=360; i+=15) {
+    glNormal3f(Sin(i), Cos(i), 0);
+    glVertex3d(r*Sin(i),r*Cos(i), 0);
+  }
+  glEnd();
+  glBegin(GL_TRIANGLE_FAN);
+  glNormal3f(0,0,-1);
+  glVertex3d(0,0,0);
+  for(i = 0; i <=360; i+=15) {
+    glVertex3d(r*Sin(i),r*Cos(i), 0);
+  }
+  glEnd();
 }
 
 /*
@@ -67,9 +99,31 @@ void key(unsigned char ch,int x,int y)
    {
       w -= 0.1;
    }
-   else if(ch == 's') {
+   else if(ch == '1') {
      shouldMove=!shouldMove;
    }
+   //First person movement requires a perspective projection - better to leave it until next assignment, but you'd probably start like this:
+   /*else if(ch == 'w') {
+     glPushMatrix();
+     glLoadIdentity();
+     double x;
+     double y;
+     double z;
+     sphericalToCartesian(1, ph, (180-th), &x, &y, &z);
+     glTranslated(x,y,z);
+     printf("Translating by vector (%f,%f,%f)\n", x, y, z);
+     glPopMatrix();
+   }
+   else if(ch == 's') {
+
+   }
+   else if(ch == 'a') {
+
+   }
+   else if(ch == 'd') {
+
+   }*/
+
 
    Project(0,w2h,dim);
    //  Tell GLUT it is necessary to redisplay the scene
@@ -132,7 +186,7 @@ void drawCube() {
     glVertex3d(-1.0f, 1.0f, 1.0f);
     glVertex3d(-1.0f, 1.0f,-1.0f);
 
-    glNormal3f(0,0,-1);
+    glNormal3f(0,-1,0);
     glColor3f(1,0,0); //red
     glVertex3d(1.0f, 1.0f,-1.0f);
     glVertex3d(-1.0f,-1.0f,-1.0f);
@@ -141,6 +195,7 @@ void drawCube() {
     glVertex3d(1.0f,-1.0f,-1.0f);
     glVertex3d(-1.0f,-1.0f,-1.0f);
 
+    glNormal3f(0,0,-1);
     glColor3f(0,1,0); //green
     glVertex3d(1.0f,-1.0f, 1.0f);
     glVertex3d(-1.0f,-1.0f,-1.0f);
@@ -149,7 +204,8 @@ void drawCube() {
     glVertex3d(-1.0f,-1.0f, 1.0f);
     glVertex3d(-1.0f,-1.0f,-1.0f);
 
-    glColor3f(0,0,1); //blue correct
+    glNormal3f(1,0,0);
+    glColor3f(0,0,1); //blue
     glVertex3d(-1.0f, 1.0f, 1.0f);
     glVertex3d(-1.0f,-1.0f, 1.0f);
     glVertex3d(1.0f,-1.0f, 1.0f);
@@ -157,6 +213,7 @@ void drawCube() {
     glVertex3d(-1.0f, 1.0f, 1.0f);
     glVertex3d(1.0f,-1.0f, 1.0f);
 
+    glNormal3f(0,1,0);
     glColor3f(1,0,1); //magenta
     glVertex3d(1.0f,-1.0f,-1.0f);
     glVertex3d(1.0f, 1.0f, 1.0f);
@@ -165,6 +222,7 @@ void drawCube() {
     glVertex3d(1.0f,-1.0f,-1.0f);
     glVertex3d(1.0f, 1.0f,-1.0f);
 
+    glNormal3f(0,0,1);
     glColor3f(0,1,1); //cyan correct
     glVertex3d(1.0f, 1.0f, 1.0f);
     glVertex3d(-1.0f, 1.0f,-1.0f);
@@ -190,6 +248,29 @@ void drawAxes(){
   glEnd();
 }
 
+void setSpineAngles(double * arr, int size) {
+  srand(time(NULL));
+  int i;
+  for(i = 0; i < size; i++) {
+    if(i%4) arr[i] = (double)(rand()%360-180);
+    else arr[i] = (double)((rand()%100)-50);
+  }
+  spineAnglesSet = 1;
+}
+
+void drawSeaUrchin(){
+  int i;
+  if(!spineAnglesSet) setSpineAngles(spineAngles, SPINES*4);
+  for(i = 0; i < SPINES; i++) {
+    glPushMatrix();
+    double spineAngle[4] = {spineAngles[i], spineAngles[i+1], spineAngles[i+2], spineAngles[i+3]};
+    glRotated(spineAngle[0], spineAngle[1], spineAngle[2], spineAngle[3]);
+    drawCone(1, 0.05);
+    glPopMatrix();
+  }
+  //glutSolidSphere(0.7, 15, 15);
+}
+
 void display() {
   //printf("display \n");
 
@@ -202,6 +283,8 @@ void display() {
   float Shinyness[] = {16};
   //This needs to be called before glClear for reasons unknown!!!!!!!!!!!
   glEnable(GL_DEPTH_TEST);
+  glEnable(GL_NORMALIZE); //Normalizes all the normal vectors.
+  glEnable(GL_LIGHTING);
   //  Clear the image
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   //  Reset previous transforms
@@ -210,8 +293,6 @@ void display() {
   glRotated(ph,1,0,0);
   glRotated(th,0,1,0);
 
-  glEnable(GL_NORMALIZE); //Normalizes all the normal vectors.
-  glEnable(GL_LIGHTING);
   //  Enable light 0
   glEnable(GL_LIGHT0);
   //  Set ambient, diffuse, specular components and position of light 0
@@ -226,22 +307,30 @@ void display() {
   glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,Specular);
   glMaterialfv(GL_FRONT_AND_BACK,GL_EMISSION,Emission);
 
-  //Draw elf dude!
-  glPushMatrix();
+  //Draw elf dude! Not for this project :(
+  /*glPushMatrix();
   glRotated(180,0,1,0);
   glScaled(0.1,0.1,0.1);
   glCallList(obj);
+  glPopMatrix();*/
+  drawAxes();
+
+  glPushMatrix();
+  drawSeaUrchin();
   glPopMatrix();
-  //Allow regular color to work again
-  glDisable(GL_LIGHTING);
+
+  //Allow regular color to work again:
+  //glDisable(GL_LIGHTING);
   //Draw some cubes. Why don't the lighting and mats etc. apply here if GL_LIGHTING is still enabled?
+  //Because they didn't have normals, smart cookie.
   double rad = 1.6;
   double i;
   //printf("cubeRotate is %f \n", cubeRotate);
   for(i = 0; i < (2*PI); i+=(PI/4)) {
     glPushMatrix();
     glTranslated(rad*cos(i),rad*sin(i),0);
-    glScaled(0.1,0.1,0.1);
+    double scalar = fabs(0.05*Sin(cubeRotate+(180*i/PI)))+0.1;
+    glScaled(scalar, scalar, scalar);
     glRotated(cubeRotate, cos(i), sin(i), 0);
     drawCube();
     glPopMatrix();
